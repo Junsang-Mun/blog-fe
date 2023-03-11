40bd7537d7f0bceaa29c512298190f6a8511d493
Junsang Moon <moon@Junsangs-MacBook-Air.local>
2023-03-11 18:49:28 +0900

    [add] 에디터 추가 (/admin)
    - tui editor 추가 완료

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,11 +1,15 @@
 <script>
+	// @ts-nocheck
 	import { onMount } from 'svelte';
 	import Editor from '@toast-ui/editor';
 	import '@toast-ui/editor/dist/toastui-editor.css';
 	import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
-	let texteditor;
-	let toastEditor;
-	
+	let texteditor, toastEditor;
+	let currentDate = new Date();
+	const offset = currentDate.getTimezoneOffset();
+	currentDate = new Date(currentDate.getTime() - (offset * 60 * 1000));
+	let pTitle, pCategory, pDate = currentDate.toISOString().split('T')[0];
+
 	onMount(() => {
 		toastEditor = new Editor({
 			el: texteditor,
@@ -15,8 +19,17 @@
 			theme: 'dark',
 		})
 	})
+
+	function submitNewPost() {
+		console.log(`${pTitle}\n${pCategory}\n${pDate}`);
+		console.log(toastEditor.getHTML());
+	}
 </script>
 
-<div id="editorWrapper">
-	<div bind:this={texteditor}></div>
-</div>
+<h1>글 쓰기</h1>
+<input type="text" placeholder="제목" class="form" bind:value={pTitle}>
+<input type="text" placeholder="카테고리" class="form" bind:value={pCategory}>
+<input type="date" placeholder="작성일" class="form" bind:value={pDate}>
+<div bind:this={texteditor}></div>
+<br>
+<button on:click={() => submitNewPost()} class="formsub">저장하기</button>
