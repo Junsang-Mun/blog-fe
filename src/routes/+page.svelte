4966d51eecae4772edca8b17c06fa8cacf318b57
Junsang Mun <mun.js@aol.com>
2023-09-23 11:49:30 +0900

    [feat] migrate to carbon-components-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,108 @@
-<style>
-	div {
-		margin-top: 3rem;
-		padding: 1rem;
-		margin-left:16.5rem;
-		height: calc(100% - 3rem);
-	}
-</style>
+<script>
+	import 'carbon-components-svelte/css/g80.css';
+	import {
+		Header,
+		HeaderUtilities,
+		HeaderNav,
+		HeaderNavItem,
+		HeaderNavMenu,
+		HeaderAction,
+		HeaderPanelLinks,
+		HeaderPanelDivider,
+		HeaderPanelLink,
+		SideNav,
+		SideNavItems,
+		SideNavMenu,
+		SideNavMenuItem,
+		SideNavLink,
+		SkipToContent,
+		Content,
+		Grid,
+		Row,
+		Column,
+		TileGroup,
+		RadioTile,
 
-<cds-header aria-label="준상이의 개발바닥">
-	<cds-header-menu-button button-label-active="Close menu" button-label-inactive="Open menu" />
-	<cds-header-name href="javascript:void 0">💻개발바닥🐶</cds-header-name>
-	<cds-header-nav menu-bar-label="💻개발바닥🐶">
-		<cds-header-nav-item href="https://junsang.dev">💻개발바닥🐶 홈</cds-header-nav-item>
-		<cds-header-nav-item href="javascript:void 0">최근 포스트</cds-header-nav-item>
-		<cds-header-menu menu-label="Contacts" trigger-content="Contacts">
-			<cds-header-menu-item href="mailto:mun.js@aol.com">Mail</cds-header-menu-item>
-			<cds-header-menu-item href="https://github.com/Junsang-Mun">GitHub</cds-header-menu-item>
-			<cds-header-menu-item
-				href="https://www.linkedin.com/in/%EC%A4%80%EC%83%81-%EB%AC%B8-b567b027a/"
-				>LinkedIn</cds-header-menu-item
-			>
-		</cds-header-menu>
-	</cds-header-nav>
-</cds-header>
+		SideNavDivider
 
-<div>
-	<h1>asdfasdf</h1>
-</div>
+	} from 'carbon-components-svelte';
+	import { expoIn } from 'svelte/easing';
 
-<cds-side-nav aria-label="Side navigation" expanded=true>
-	<cds-side-nav-items>
-		<cds-side-nav-link href="javascript:void(0)">개발 🐶🐾</cds-side-nav-link>
-		<cds-side-nav-link href="javascript:void(0)">커뮤니티 🎤</cds-side-nav-link>
-		<cds-side-nav-link href="javascript:void(0)">일상 🏡</cds-side-nav-link>
-		<cds-side-nav-divider />
-		<cds-side-nav-link href="javascript:void(0)">검색 🔎</cds-side-nav-link>
-	</cds-side-nav-items>
-</cds-side-nav>
+	let isSideNavOpen = false;
+	let isOpen = false;
+	let selected = '0';
+	let transitions = {
+		'0': {
+			text: 'Default (duration: 200ms)',
+			value: { duration: 200 }
+		},
+		'1': {
+			text: 'Custom (duration: 600ms, delay: 50ms, easing: expoIn)',
+			value: { duration: 600, delay: 50, easing: expoIn }
+		},
+		'2': {
+			text: 'Disabled',
+			value: false
+		}
+	};
+</script>
+
+<Header platformName="💻개발바닥🐶" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderNav>
+		<HeaderNavItem href="/" text="💻개발바닥🐶 홈" />
+		<HeaderNavItem href="/" text="최근 포스트" />
+		<HeaderNavMenu text="Contacts" >
+			<HeaderNavItem href="mailto:mun.js@aol.com" text="Mail" />
+			<HeaderNavItem href="https://github.com/Junsang-Mun" text="GitHub" />
+			<HeaderNavItem href="https://www.linkedin.com/in/%EC%A4%80%EC%83%81-%EB%AC%B8-b567b027a/" text="LinkedIn" />
+		</HeaderNavMenu>
+	</HeaderNav>
+	<HeaderUtilities>
+	</HeaderUtilities>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+	<SideNavItems>
+		<SideNavLink text="개발 🐶🐾" />
+		<SideNavLink text="커뮤니티 🎤" />
+		<SideNavLink text="일상 🏡" />
+		<SideNavDivider />
+		<SideNavLink text="검색 🔎" />
+	</SideNavItems>
+</SideNav>
+
+<Content>
+	<slot>
+		<h1>💻 개발바닥 🐶</h1>
+		<br/>
+		<p>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.
+			Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+			Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
+			Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+			Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
+			Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+		</p>
+		<br/>
+		<p>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.
+			Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+			Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
+			Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+			Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
+			Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+		</p>
+		<br/>
+		<p>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa.
+			Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+			Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
+			Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+			Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
+			Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+		</p>
+	</slot>
+</Content>
