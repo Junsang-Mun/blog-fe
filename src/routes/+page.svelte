b86cd8123c89568956e2959940775cb2e2b87ff7
Junsang Mun <mun.js@aol.com>
2023-09-23 11:20:08 +0900

    [feat] wireframe work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,39 @@
-<script>
-	function openUrl(url) {
-		window.open(url, "_blank");
+<style>
+	div {
+		margin-top: 3rem;
+		padding: 1rem;
+		margin-left:16.5rem;
+		height: calc(100% - 3rem);
 	}
-</script>
+</style>
 
-<div class="page">
-	<div id="main">
-		<h1 id="title">제주고양이 - ChejuCat</h1>
-		<div id="outlinks">
-			<button class="social" id="linkedin" on:click={() => openUrl("https://www.linkedin.com/in/%EC%A4%80%EC%83%81-%EB%AC%B8-b567b027a/")}><img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" alt="To My LinkedIn Profile"/></button>
-			<button class="social round" id="github" on:click={() => openUrl("https://github.com/Junsang-Mun")}><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="To My GitHub Profile"/></button>
-			<button class="social round" id="facebook" on:click={() => openUrl("https://www.facebook.com/profile.php?id=100032805280715")}><img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="To My Facebook Profile"/></button>
-		</div>
-	</div>
+<cds-header aria-label="준상이의 개발바닥">
+	<cds-header-menu-button button-label-active="Close menu" button-label-inactive="Open menu" />
+	<cds-header-name href="javascript:void 0">💻개발바닥🐶</cds-header-name>
+	<cds-header-nav menu-bar-label="💻개발바닥🐶">
+		<cds-header-nav-item href="https://junsang.dev">💻개발바닥🐶 홈</cds-header-nav-item>
+		<cds-header-nav-item href="javascript:void 0">최근 포스트</cds-header-nav-item>
+		<cds-header-menu menu-label="Contacts" trigger-content="Contacts">
+			<cds-header-menu-item href="mailto:mun.js@aol.com">Mail</cds-header-menu-item>
+			<cds-header-menu-item href="https://github.com/Junsang-Mun">GitHub</cds-header-menu-item>
+			<cds-header-menu-item
+				href="https://www.linkedin.com/in/%EC%A4%80%EC%83%81-%EB%AC%B8-b567b027a/"
+				>LinkedIn</cds-header-menu-item
+			>
+		</cds-header-menu>
+	</cds-header-nav>
+</cds-header>
+
+<div>
+	<h1>asdfasdf</h1>
 </div>
+
+<cds-side-nav aria-label="Side navigation" expanded=true>
+	<cds-side-nav-items>
+		<cds-side-nav-link href="javascript:void(0)">개발 🐶🐾</cds-side-nav-link>
+		<cds-side-nav-link href="javascript:void(0)">커뮤니티 🎤</cds-side-nav-link>
+		<cds-side-nav-link href="javascript:void(0)">일상 🏡</cds-side-nav-link>
+		<cds-side-nav-divider />
+		<cds-side-nav-link href="javascript:void(0)">검색 🔎</cds-side-nav-link>
+	</cds-side-nav-items>
+</cds-side-nav>
