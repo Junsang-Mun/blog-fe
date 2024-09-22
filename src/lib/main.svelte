de56d84d6917db00e6af02984f327d1a58bb9ba1
Junsang Mun <mun.js@aol.com>
2024-09-22 19:49:34 +0900

    Initial commit

--- src/lib/main.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import 'carbon-components-svelte/css/g80.css';
-</script>
-
-<h1>💻 개발바닥 🐶</h1>
-<br/>
-<p>개발바닥은 기록 저장소입니다.</p>
-<p>사람은 똑같은 실수를 반복하지만, 기록이 실수를 줄이는 데 도움이 된다고 믿습니다.</p>
--- src/routes/api/+server.js
+++
@@ -1,5 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-export function GET({ url }) {
-	throw error(400, 'Are you in the right place?');
-}
